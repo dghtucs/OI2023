06018828de6429cc5f32684e21a6e0d51de8d6ed
dgh <tudghian2020@qq.com>
2023-07-10 14:42:40 +0800

    1

--- 实习/ship/urls.py
+++ 实习/ship/urls.py
@@ -5,7 +5,7 @@ from urllib.parse import urljoin
 headers = {
     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36",
 }
-url = "https://www.cnss.com.cn/html/cbzsk/index_4.html"
+url = "https://www.cnss.com.cn/html/cbzsk/index_6.html"
 
 def get_all_links(url):
     response = requests.get(url,headers=headers)
