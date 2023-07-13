e8d917d24504a3bedd13dbe7fa17d12039445c13
dgh <tudghian2020@qq.com>
2023-07-14 00:33:31 +0800

    Update main.py

--- 实习/ship/main.py
+++ 实习/ship/main.py
@@ -1,7 +1,7 @@
 import requests
 from bs4 import BeautifulSoup
 from urllib.parse import urljoin
-
+# ((&&YsNn:N9jN'!
 headers = {
     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36",
 }
