a2f5078bbd2d3227bc5b64e1ecb3e8ae472137a6
dgh <tudghian2020@qq.com>
2023-07-14 16:56:15 +0800

    Update x2json.py

--- 实习/excel/x2json.py
+++ 实习/excel/x2json.py
@@ -6,5 +6,4 @@ df = pd.read_excel('1.xlsx',dtype={'code':str})
 # 保存各列数据
 for column in df.columns:
     column_data = df[column]
-    print(column_data)
     column_data.to_csv(f'{column}.csv', index=False)
