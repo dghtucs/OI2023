2a656f1ce29bbf5356ee1b320f489ea1ff16d718
dgh <tudghian2020@qq.com>
2023-07-23 21:26:09 +0800

    1

---
+++ 信息学奥赛一本通/c08Pointer/c01test.cpp
@@ -0,0 +1,43 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int a= 3;
+    cin >> a;
+    int* p = &a;
+    cout << p << endl;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ 信息学奥赛一本通/c08Pointer/c06Link/c01test.cpp
@@ -0,0 +1,61 @@
+#include<iostream>
+using namespace std;
+
+struct Node
+{
+    /* data */
+    int data;
+    Node* next;
+};
+
+int main()
+{
+    Node* head = new Node;
+    Node* p,*q;
+    int x;
+    cin >> x;
+    p = head;
+    while (x != -1)
+    {
+        /* code */
+        q = new Node;
+        q->data = x;
+        q->next = nullptr;
+        p->next = q;
+        p = q;
+        cin >> x;
+    }
+    p = head->next;
+    while (p->next != nullptr)
+    {
+        /* code */
+        cout << p->data << " -> ";
+        p = p->next;
+    }
+    
+    
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
